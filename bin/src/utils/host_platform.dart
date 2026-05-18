8a74f64c2e6f7feea5ac797cc7af31d4b2f24134
Thomas.Duffin <thomas.duffin@mearsgroup.co.uk>
2026-05-18 13:42:14 +0100

    refactor: host platform refactored to use package:platform for testability. Expose static "overridePlatform" method to enable testability. Added package:meta for @visibleForTesting annotation.

--- bin/src/utils/host_platform.dart
+++ bin/src/utils/host_platform.dart
@@ -1,35 +1,51 @@
-import 'dart:io';
 import 'exceptions.dart';
+import 'package:platform/platform.dart';
+import 'package:meta/meta.dart';
+
+/// Wrapper around [Platform] for testing.
+Platform _platform = LocalPlatform();
 
 /// Operating System-specific constants for terminal commands, directories, etc.
 ///
-/// Supports Windows, Mac, and Linux hosts. Others will throw an [UnknownPlatformException].
+/// Supports Windows, Mac, and Linux hosts. Others will throw an
+/// [UnknownPlatformException].
 abstract class HostPlatformConfig {
+  @visibleForTesting
+  static void overridePlatform(Platform platform) {
+    _platform = platform;
+  }
+
   /// Returns the expected location for the dart pub cache.
   ///
   /// * For Unix systems this is usually a hidden folder in your home directory.
   /// * For Windows this is usually in your AppData folder.
   static String get pubCacheLocation {
-    if (Platform.isWindows) {
-      return '${Platform.environment['USERPROFILE']}\\AppData\\Local\\Pub\\Cache\\bin';
-    } else if (Platform.isMacOS || Platform.isLinux) {
-      return '${Platform.environment['HOME']}/.pub/cache/bin';
-    } else {
-      throw UnknownPlatformException();
+    switch (_platform.operatingSystem) {
+      case 'linux':
+      case 'macos':
+        return '${_platform.environment['HOME']}/.pub/cache/bin';
+      case 'windows':
+        return '${_platform.environment['USERPROFILE']}'
+            '\\AppData\\Local\\Pub\\Cache\\bin';
+      case _:
+        throw UnknownPlatformException();
     }
   }
 
   /// Returns the expected name of the `flutterfire_cli` command.
   ///
   /// * For Unix systems this is usually just `flutterfire`.
-  /// * For Windows this is usually `flutterfire.bat`, but powershell terminals allow the Unix method, too.
+  /// * For Windows this is usually `flutterfire.bat`, but powershell terminals
+  ///   allow the Unix method, too.
   static String get flutterfireCli {
-    if (Platform.isWindows) {
-      return 'flutterfire.bat';
-    } else if (Platform.isMacOS || Platform.isLinux) {
-      return 'flutterfire';
-    } else {
-      throw UnknownPlatformException();
+    switch (_platform.operatingSystem) {
+      case 'linux':
+      case 'macos':
+        return 'flutterfire';
+      case 'windows':
+        return 'flutterfire.bat';
+      case _:
+        throw UnknownPlatformException();
     }
   }
 }
--- test/utils/host_platform_test.dart
+++ test/utils/host_platform_test.dart
@@ -1,14 +1,34 @@
 import 'package:platform/platform.dart';
-
 import 'package:test/test.dart';
 
 import '../../bin/src/utils/host_platform.dart';
 
 void main() {
-  test('configuration changes based on platform', () {
-    // TODO override platform.
+  test('flutterfire config changes based on platform', () {
+    HostPlatformConfig.overridePlatform(FakePlatform(operatingSystem: 'macos'));
     expect(HostPlatformConfig.flutterfireCli, equals('flutterfire'));
-    // TODO override platform.
+
+    HostPlatformConfig.overridePlatform(
+      FakePlatform(operatingSystem: 'windows'),
+    );
     expect(HostPlatformConfig.flutterfireCli, equals('flutterfire.bat'));
+  });
+
+  test('pubspec cache config changes based on platform', () {
+    HostPlatformConfig.overridePlatform(
+      FakePlatform(operatingSystem: 'linux', environment: {'HOME': '/home/'}),
+    );
+    expect(HostPlatformConfig.pubCacheLocation, endsWith('/.pub/cache/bin'));
+
+    HostPlatformConfig.overridePlatform(
+      FakePlatform(
+        operatingSystem: 'windows',
+        environment: {'USERPROFILE': 'C:\\Users\\User.Name\\'},
+      ),
+    );
+    expect(
+      HostPlatformConfig.pubCacheLocation,
+      endsWith('\\AppData\\Local\\Pub\\Cache\\bin'),
+    );
   });
 }
